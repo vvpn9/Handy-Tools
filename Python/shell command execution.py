ca98b4b8b83a57e0ee91fd56e657a876c12500ad
vvpn9 <moonshake@foxmail.com>
2021-03-09 17:03:19 +0800

    wip

--- Python/shell command execution.py
+++ Python/shell command execution.py
@@ -5,7 +5,6 @@
 # 如下方法缺点在于不能将输出结果作为变量
 
 import os
-import pathlib
 import shlex
 import subprocess
 
@@ -93,3 +92,27 @@ print(process.stdout)
 # for line in ssh.stdout:
 #     print(line.strip())
 
+
+# os.listdir()用于返回一个由文件名和目录名组成的列表
+#
+# os.path.isdir()用于判断对象是否为一个目录
+#
+# os.path.isfile()用于判断对象是否为一个文件
+
+# os.path.join()函数：连接两个或更多的路径名组件
+#
+# 1.如果各组件名首字母不包含’/’，则函数会自动加上
+# 2.如果有一个组件是一个绝对路径，则在它之前的所有组件均会被舍弃
+# 3.如果最后一个组件为空，则生成的路径以一个’/’分隔符结尾
+#
+# Demo1
+# import os
+#
+# Path1 = 'home'
+# Path2 = 'develop'
+# Path3 = 'code'
+#
+# Path10 = Path1 + Path2 + Path3
+# Path20 = os.path.join(Path1,Path2,Path3)
+# print ('Path10 = ',Path10)
+# print ('Path20 = ',Path20)
