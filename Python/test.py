ce699efe2cb2a285f145e00b21efb9df58cff1bf
vvpn9 <moonshake@foxmail.com>
2021-03-10 17:54:27 +0800

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,2 +1,7 @@
-a = [1, 2, 3, 4, 5]
-print(a[-3:])
+str8 = "3万"
+# a=str8.find("Python")
+# print a
+# b = str8.find("和")
+# print(b)
+word = str8.split("万")  # Python3 和Spark 这里可以直接正确分割中文
+print(word)
